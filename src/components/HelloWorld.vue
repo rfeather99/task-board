e3afe5c4b37a5b6cfb2cb2f1536e9d64ab879784
@rfeather99 <rfeather99@yahoo.co.jp>
2020-04-12 17:07:46 +0900

    :sparkles: 画面の雛形を作成

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,86 +1,42 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br />
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener"
-        >vue-cli documentation</a
-      >.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-typescript"
-          target="_blank"
-          rel="noopener"
-          >typescript</a
-        >
+
+    <ul class="kanban-board">
+      <li class="status">
+        <ul class="ul01">
+          <li class="li00">PBI</li>
+          <li
+            v-for="(status, index) in statusses"
+            :key="status"
+            :class="'li0' + (index + 1)"
+          >
+            {{ status }}
+          </li>
+        </ul>
       </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint"
-          target="_blank"
-          rel="noopener"
-          >eslint</a
-        >
-      </li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li>
-        <a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a>
-      </li>
-      <li>
-        <a href="https://forum.vuejs.org" target="_blank" rel="noopener"
-          >Forum</a
-        >
-      </li>
-      <li>
-        <a href="https://chat.vuejs.org" target="_blank" rel="noopener"
-          >Community Chat</a
-        >
-      </li>
-      <li>
-        <a href="https://twitter.com/vuejs" target="_blank" rel="noopener"
-          >Twitter</a
-        >
-      </li>
-      <li>
-        <a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a>
-      </li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li>
-        <a href="https://router.vuejs.org" target="_blank" rel="noopener"
-          >vue-router</a
-        >
-      </li>
-      <li>
-        <a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-devtools#vue-devtools"
-          target="_blank"
-          rel="noopener"
-          >vue-devtools</a
-        >
-      </li>
-      <li>
-        <a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener"
-          >vue-loader</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-          rel="noopener"
-          >awesome-vue</a
-        >
+      <li class="tasks">
+        <ul v-for="pbi in pbis" :key="pbi.id">
+          <li class="li00">
+            <div class="pbi">
+              <h3>{{ pbi.title }}</h3>
+            </div>
+          </li>
+          <li
+            v-for="(status, index) in statusses"
+            :class="'li0' + (index + 1)"
+            :key="status"
+          >
+            <div
+              v-for="task in pbi.tasks.filter(t => t.status == status)"
+              :key="task.id"
+              class="task"
+            >
+              <h3>{{ task.title }}</h3>
+              <p>{{ task.description }}</p>
+            </div>
+          </li>
+        </ul>
       </li>
     </ul>
   </div>
@@ -89,26 +45,217 @@
 <script lang="ts">
 import { Component, Prop, Vue } from "vue-property-decorator";
 
+type PBI = {
+  id: number;
+  title: string;
+  tasks: Array<Task>;
+};
+type Task = {
+  id: number;
+  title: string;
+  description: string;
+  assignee: string | null;
+  status: string;
+  point: number | null;
+};
+
 @Component
 export default class HelloWorld extends Vue {
   @Prop() private msg!: string;
+  private statusses: Array<string> = ["TODO", "DOING", "REVIEW", "DONE"];
+  private pbis: Array<PBI> = [
+    {
+      id: 0,
+      title: "一つ目",
+      tasks: [
+        {
+          id: 0,
+          title: "あああああ",
+          description: "あああああ",
+          assignee: null,
+          status: "TODO",
+          point: 1.0
+        },
+        {
+          id: 1,
+          title: "あああああ",
+          description: "あああああ",
+          assignee: "山田一浪",
+          status: "DOING",
+          point: 0.5
+        },
+        {
+          id: 2,
+          title: "あああああ",
+          description: "あああああ",
+          assignee: "山田一浪",
+          status: "DONE",
+          point: 1.0
+        }
+      ]
+    },
+    {
+      id: 1,
+      title: "二つ目",
+      tasks: [
+        {
+          id: 3,
+          title: "2あああああ",
+          description: "あああああ",
+          assignee: null,
+          status: "TODO",
+          point: 1.0
+        },
+        {
+          id: 4,
+          title: "2あああああ",
+          description: "あああああ",
+          assignee: null,
+          status: "TODO",
+          point: 2.0
+        },
+        {
+          id: 5,
+          title: "2あああああ",
+          description: "あああああ",
+          assignee: "山田一浪",
+          status: "REVIEW",
+          point: 1.0
+        },
+        {
+          id: 6,
+          title: "2あああああ",
+          description: "あああああ",
+          assignee: "山田一浪",
+          status: "DONE",
+          point: 1.0
+        }
+      ]
+    },
+    {
+      id: 2,
+      title: "三つ目",
+      tasks: [
+        {
+          id: 7,
+          title: "3あああああ",
+          description: "あああああ",
+          assignee: null,
+          status: "TODO",
+          point: 1.0
+        },
+        {
+          id: 8,
+          title: "3あああああ",
+          description: "あああああ",
+          assignee: null,
+          status: "TODO",
+          point: 2.0
+        },
+        {
+          id: 9,
+          title: "3あああああ",
+          description: "あああああ",
+          assignee: "山田一浪",
+          status: "REVIEW",
+          point: 1.0
+        },
+        {
+          id: 10,
+          title: "3あああああ",
+          description: "あああああ",
+          assignee: "山田一浪",
+          status: "REVIEW",
+          point: 1.0
+        }
+      ]
+    },
+    {
+      id: 3,
+      title: "四つ目",
+      tasks: [
+        {
+          id: 11,
+          title: "4あああああ",
+          description: "あああああ",
+          assignee: null,
+          status: "TODO",
+          point: 1.0
+        },
+        {
+          id: 12,
+          title: "4あああああ",
+          description: "あああああ",
+          assignee: null,
+          status: "TODO",
+          point: 2.0
+        },
+        {
+          id: 13,
+          title: "4あああああ",
+          description: "あああああ",
+          assignee: "山田一浪",
+          status: "REVIEW",
+          point: 1.0
+        },
+        {
+          id: 14,
+          title: "4あああああ",
+          description: "あああああ",
+          assignee: "山田一浪",
+          status: "REVIEW",
+          point: 1.0
+        }
+      ]
+    }
+  ];
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
 ul {
   list-style-type: none;
-  padding: 0;
+  padding-left: 0px;
 }
 li {
-  display: inline-block;
-  margin: 0 10px;
+  margin-left: 5px;
+  margin-right: 5px;
 }
-a {
-  color: #42b983;
+ul.kanban-board {
+  width: 100%;
+}
+ul.kanban-board ul {
+  display: flex;
+}
+ul.kanban-board ul.ul01 {
+  border-bottom: 0;
+}
+ul.kanban-board ul li {
+  flex-grow: 1;
+  width: 25%;
+  box-sizing: border-box;
+  border-top: 1px solid #ccc;
+  border-right: 1px solid #ccc;
+  border-left: 1px solid #ccc;
+  text-align: center;
+  padding: 10px;
+}
+.tasks ul:last-child li {
+  border-bottom: 1px solid #ccc;
+}
+ul.kanban-board ul.ul01 li {
+  background-color: #ffeaea;
+}
+ul.kanban-board ul.ul01 li.li00 {
+  background-color: #ff9191;
+}
+.pbi {
+  margin: 1px;
+  border: 1px solid #ccc;
+}
+.task {
+  margin: 1px;
+  border: 1px solid #ccc;
 }
 </style>
